2bd5079aaef94ea800cce9830bac3c8b147263be
wyattsweet <wyattsweet@gmail.com>
2018-08-13 15:02:07 -0700

    updated readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Builder
+
+A simple CLI for maintaining build config files for React apps.
--- bin/linkBabelrc.js
+++ bin/linkBabelrc.js
@@ -1,9 +1,10 @@
 #! /usr/bin/env node
-const shell = require('shelljs');
-const path = require('path');
+const shell = require('shelljs')
+const path = require('path')
 
-const currentDir = shell.pwd();
-const configFile = path.join(__dirname, '../src/babelrc');
+const currentDir = shell.pwd()
+const configFile = path.join(__dirname, '../src/babelrc')
 
-shell.ln('-s', configFile, `${currentDir}/.babelrc`);
-shell.exec(`echo linked babel config`);
+shell.ln('-s', configFile, `${currentDir}/.babelrc`)
+shell.exec(`echo linked babel config`)
+console.log('done')
